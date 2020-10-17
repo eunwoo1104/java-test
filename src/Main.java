4206b1c7dcb37afea43b6fb52bc36124557f62ad
eunwoo1104 <sions04@naver.com>
2020-10-17 16:56:42 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,27 @@ import static java.lang.System.out;
 public class Main
 {
     private final Importable rand = new Importable();
+    private final int[] list = new int[10];
     public static void main(String[] args) {
         Main Main = new Main();
         Main.idk();
+        pr(test());
+        Main.howToForEach();
     }
 
     public void idk(){
         int a = this.rand.refreshNum();
         for(int i = 0; i<10; i++){
+            int _a = this.rand.refreshNum();
+            this.list[i] = _a;
             pr(Integer.toString(this.rand.num));
-            this.rand.refreshNum();
         }
-        pr(Integer.toString(a));
+    }
+
+    public void howToForEach(){
+        for(int rand: this.list){
+            pr(Integer.toString(rand));
+        }
     }
 
     public static String test()
